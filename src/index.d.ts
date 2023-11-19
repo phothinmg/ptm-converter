ea8da52b36dd292dbd424f6c2262bf0749621ca6
phothinmg <phothinmg@gmail.com>
2023-11-20 05:56:37 +0630

    new file:   src/index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+export { Fs as default };
+declare class Fs {
+    constructor(e: any);
+    filePath: any;
+    formatDate(e: any): string;
+    readTime(e: any): number;
+    lastUpdate(): any;
+    convert(): any;
+    filecontent(): {
+        data: any;
+        content: any;
+    };
+    get data(): any;
+    get content(): any;
+    get postTitle(): any;
+    get postDate(): string;
+    get lastUpdatedDate(): string;
+    get readingTime(): number;
+    get json(): string;
+    get convertedContent(): any;
+    get postHtml(): string;
+    get pageHtml(): string;
+}
+//# sourceMappingURL=index.d.ts.map
